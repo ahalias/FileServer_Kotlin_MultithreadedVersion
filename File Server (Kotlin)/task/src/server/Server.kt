fdae8506a89f382a19b15ba3ace807aebdd035d5
ahalias <120400735+ahalias@users.noreply.github.com>
2023-04-26 11:40:28 +0300

    Update Server.kt
    multithreading

--- File Server (Kotlin)/task/src/server/Server.kt
+++ File Server (Kotlin)/task/src/server/Server.kt
@@ -3,112 +3,111 @@ package server
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.File
+import java.io.IOException
 import java.net.InetAddress
 import java.net.ServerSocket
 import kotlin.random.Random
 import kotlin.system.exitProcess
-import java.util.concurrent.Executors
-
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-
-        val executor = Executors.newFixedThreadPool(10)
-
-
         val address = "127.0.0.1"
         val port = 23456
+        try {
+            ServerSocket(port, 50, InetAddress.getByName(address)).use { server ->
+                print("Server Started")
+                while (true) {
+                    val session = Session(server)
+                    session.start()
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 
-        val server = ServerSocket(port, 50, InetAddress.getByName(address))
-        println("Server started!")
 
-        val filePath = "src/server/data/"
-        val idPath = "src/server/data/id/"
 
-        var name = ""
+    class Session(private val server: ServerSocket) : Thread() {
+        private val socket = server.accept()
+        private val input = DataInputStream(socket.getInputStream())
+        private val output = DataOutputStream(socket.getOutputStream())
+        private val filePath = "src/server/data/"
+        private val idPath = "src/server/data/id/"
+        private var getName = ""
 
-        fun getRandomName(): String {
+        override fun run() {
+            try {
+                input.use { input ->
+                    output.use { output ->
+                        val serverMessage = input.readUTF().split(" ", limit = 3)
+                        if (serverMessage[0] == "EXIT") {
+                            server.close()
+                            exitProcess(1)
+                        } else {
+                            when (serverMessage[0]) {
+                                "PUT" -> createFile(serverMessage[1])
+                                "GET" -> sendFile(serverMessage[1], serverMessage[2])
+                                "DELETE" -> deleteFile(serverMessage[1], serverMessage[2])
+                            }
+                        }
+                        socket.close()
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        private fun getRandomName(): String {
             val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
             return (1..Random.nextInt(5, 10))
                 .map { allowedChars.random() }
                 .joinToString("")
         }
 
-        fun getFileNameById(filename: String) = File("${idPath}${filename}").readText()
+        private fun getFileNameById(filename: String) = File("${idPath}${filename}").readText()
 
-        fun getId() = Random.nextInt(1, 10000000)
-
-        while (true) {
-                val socket = server.accept()
-                val input = DataInputStream(socket.getInputStream())
-                val output = DataOutputStream(socket.getOutputStream())
-
-                fun createFile(filename: String) {
-                    try {
-                        val length = input.readInt()
-                        val message = ByteArray(length)
-                        input.readFully(message, 0, message.size)
-                        if (filename == "RANDOM") {
-                            name = getRandomName()
-                        } else {
-                            name = filename
-                        }
-                        val file = File("${filePath}${name}")
-                        file.writeBytes(message)
-                        val id = getId()
-                        val fileId = File("${idPath}${id}")
-                        fileId.writeText("$name")
-                        output.writeUTF("200 $id")
-                    } catch (e: Exception) {
-                        output.writeUTF("403")
-                    }
-                }
-
-                fun sendFile(choice: String, filename: String) {
-                    try {
-                        if (choice == "ID") {
-                            name = getFileNameById(filename)
-                        } else {
-                            name = filename
-                        }
-                        val message = File("$filePath$name").readBytes()
-                        output.writeUTF("200 $name")
-                        output.flush()
-                        output.writeInt(message.size)
-                        output.write(message)
-                    } catch (e: Exception) {
-                        output.writeUTF("404")
-                    }
-                }
-
-                fun deleteFile(choice: String, filename: String) {
-                    try {
-                        if (choice == "ID") {
-                            name = getFileNameById(filename)
-                        } else {
-                            name = filename
-                        }
-                        File("${filePath}${name}").delete()
-                        output.writeUTF("200")
-                    } catch (e: Exception) {
-                        output.writeUTF("404")
-                    }
-                }
-
-                val serverMessage = input.readUTF().split(" ", limit = 3)
-            if (serverMessage[0] ==  "EXIT") {
-                server.close()
-                exitProcess(1)
-            } else {
-
-                when (serverMessage[0]) {
-                    "PUT" -> createFile(serverMessage[1])
-                    "GET" -> sendFile(serverMessage[1], serverMessage[2])
-                    "DELETE" -> deleteFile(serverMessage[1], serverMessage[2])
-            }
-                }
-                socket.close()
+        private fun generateId() = Random.nextInt(1, 10000000)
+        private fun createFile(filename: String) {
+            try {
+                val length = input.readInt()
+                val message = ByteArray(length)
+                input.readFully(message, 0, message.size)
+                getName = if (filename == "RANDOM") getRandomName() else filename
+                val file = File("${filePath}${getName}")
+                file.writeBytes(message)
+                val id = generateId()
+                val fileId = File("${idPath}${id}")
+                fileId.writeText(getName)
+                output.writeUTF("200 $id")
+            } catch (e: Exception) {
+                output.writeUTF("403")
             }
         }
+
+        private fun sendFile(choice: String, filename: String) {
+            try {
+                getName = if (choice == "ID") getFileNameById(filename) else filename
+                val message = File("$filePath$getName").readBytes()
+                output.writeUTF("200 $getName")
+                output.flush()
+                output.writeInt(message.size)
+                output.write(message)
+            } catch (e: Exception) {
+                output.writeUTF("404")
+            }
+        }
+
+        private fun deleteFile(choice: String, filename: String) {
+            try {
+                getName = if (choice == "ID") getFileNameById(filename) else filename
+                File("${filePath}${getName}").delete()
+                output.writeUTF("200")
+            } catch (e: Exception) {
+                output.writeUTF("404")
+            }
+        }
+    }
     }
